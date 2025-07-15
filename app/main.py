38d4c10c6f514ce9c494c43e610abe8c62bdabb0
DungDaot08 <dungdaot08@gmail.com>
2025-07-15 22:55:56 +0700

    Change

--- app/main.py
+++ app/main.py
@@ -3,12 +3,25 @@ from app.api.endpoints import procedures, tickets, seats, counters, users
 from app.database import engine, Base
 from fastapi_utils.tasks import repeat_every
 from app.background.auto_call import check_and_call_next
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Kiosk API",
               root_path="/app")
+origins = [
+    "https://laysotudong.netlify.app",  # domain frontend
+    # Bạn có thể thêm localhost nếu test local:
+    "http://localhost:3000"
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,              # Chỉ cho phép domain frontend
+    allow_credentials=True,
+    allow_methods=["*"],                # Cho phép tất cả method (GET, POST,...)
+    allow_headers=["*"],                # Cho phép tất cả headers
+)
 app.include_router(procedures.router, prefix="/procedures", tags=["Procedures"])
 app.include_router(tickets.router, prefix="/tickets", tags=["Tickets"])
 app.include_router(seats.router, prefix="/seats", tags=["Seats"])
