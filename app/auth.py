6371b866823381756adf3cc2c1ef6ac1698385b4
DungDaot08 <dungdaot08@gmail.com>
2025-07-17 13:03:10 +0700

    Change

--- app/auth.py
+++ app/auth.py
@@ -21,6 +21,8 @@ pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
 
 # --- PASSWORD FUNCTIONS ---
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
