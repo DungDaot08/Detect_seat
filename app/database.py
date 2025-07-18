a5c240c0db78ce7e681b2acc3391b981ef11c127
DungDaot08 <dungdaot08@gmail.com>
2025-07-18 08:42:02 +0700

    Change

--- app/database.py
+++ app/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123@localhost/kiosk_db"
-#SQLALCHEMY_DATABASE_URL = "postgresql://detect_seat_user:ixRpspwrkGkn4ylMjo222PIDFrVJghfD@dpg-d1r60amr433s739telgg-a/detect_seat"
+#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123@localhost/kiosk_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://detect_seat_user:ixRpspwrkGkn4ylMjo222PIDFrVJghfD@dpg-d1r60amr433s739telgg-a/detect_seat"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
