0e8116bc1c70f29098813ca600e6c08b1aabd043
DungDaot08 <dungdaot08@gmail.com>
2025-08-19 15:28:17 +0700

    Change

--- app/database.py
+++ app/database.py
@@ -7,7 +7,16 @@ from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL = "postgresql://detect_seat_user:ixRpspwrkGkn4ylMjo222PIDFrVJghfD@dpg-d1r60amr433s739telgg-a.oregon-postgres.render.com/detect_seat"
 #SQLALCHEMY_DATABASE_URL = "postgresql://hcc_test_n4kv_user:yIzoeFfgAKJeCQoAtZbe5UXqIbSc1KAQ@dpg-d25cukadbo4c73bfspg0-a/hcc_test_n4kv"
 #SQLALCHEMY_DATABASE_URL = "postgresql://hcc_test_n4kv_user:yIzoeFfgAKJeCQoAtZbe5UXqIbSc1KAQ@dpg-d25cukadbo4c73bfspg0-a.oregon-postgres.render.com/hcc_test_n4kv"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+#engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_size=20,        # giữ thường trực 20 connections
+    max_overflow=30,     # có thể mở thêm tối đa 20 khi quá tải
+    pool_timeout=30,     # chờ 30s nếu pool full
+    pool_recycle=1800,   # reset sau 30 phút
+    pool_pre_ping=True   # kiểm tra connection sống
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
