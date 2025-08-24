aac610751e0dafae2ed276e82747a4432964bbbd
DungDaot08 <dungdaot08@gmail.com>
2025-08-24 22:47:26 +0700

    Change

--- app/utils/jwt_ultils.py
+++ app/utils/jwt_ultils.py
@@ -3,7 +3,7 @@ import jwt
 import time
 from typing import Dict
 
-SECRET_KEY = "super_secret_key"  # nên đặt trong config/env
+SECRET_KEY = "key"  # nên đặt trong config/env
 ALGORITHM = "HS256"
 EXPIRE_SECONDS = 60 * 30  # 30 phút
 
@@ -11,9 +11,9 @@ def create_ticket_token(data: Dict, expire_seconds: int = EXPIRE_SECONDS):
     to_encode = data.copy()
     now = int(time.time())
     to_encode.update({
-        "iat": now,
+        #"iat": now,
         "exp": now + expire_seconds,
-        "nonce": str(now)  # hoặc uuid4() để tránh reuse
+        #"nonce": str(now)  # hoặc uuid4() để tránh reuse
     })
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
